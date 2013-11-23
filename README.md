93728bf623b739dca8fac4d9e9f969a6d03a4dfe
Simo Tripodi <stripodi@adobe.com>
2013-11-23 14:15:30 +0100

    initial codebase checkin

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 connectivity
 ============
 
-Graph connectivity problem solver implementation.
+[Graph connectivity](http://en.wikipedia.org/wiki/Connectivity_(graph_theory) problem solver implementation.
+
+# Usage
+
+Given a generic `org.nnsoft.trudeau.api.UndirectedGraph<V, E>`, users can find a set of solutions by applying the [Minimum spanning tree](http://en.wikipedia.org/wiki/Minimum_spanning_tree) algorithm:
+
+```
+import static org.nnsoft.trudeau.connectivity.ConnectivitySolver.findConnectedComponent;
+
+import java.util.Collection;
+import java.util.List;
+import org.nnsoft.trudeau.api.UndirectedGraph;
+
+...
+
+UndirectedGraph<V, E> graph;
+
+Collection<List<V>> minimumSpanningTrees =
+    findConnectedComponent( graph ).includingAllVertices().applyingMinimumSpanningTreeAlgorithm();
+```
+
+Sometimes users need to include some vertices in the solutions, so they can include them by the following:
+
+```
+V a;
+V b;
+V c;
+
+Collection<List<V>> minimumSpanningTrees =
+    findConnectedComponent( graph ).includingVertices( a, b, c ).applyingMinimumSpanningTreeAlgorithm();
+```
---
+++ src/main/java/org/nnsoft/trudeau/connectivity/ConnectivityBuilder.java
@@ -0,0 +1,43 @@
+package org.nnsoft.trudeau.connectivity;
+
+/*
+ *   Copyright 2013 The Trudeau Project
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/**
+ * Builder to specify the set of vertices included into a connected component.
+ *
+ * @param <V> the Graph vertices type
+ * @param <E> the Graph edges type
+ */
+public interface ConnectivityBuilder<V, E>
+{
+
+    /**
+     * Specifies the set of vertices included into a connected component.
+     *
+     * @param vertices the set of vertices included into a connected component.
+     * @return the connectivity algorithm selector.
+     */
+    ConnectivityAlgorithmsSelector<V, E> includingVertices( V... vertices );
+
+    /**
+     * Find all the connected components included into the specified graph
+     *
+     * @return the connectivity algorithm selector.
+     */
+    ConnectivityAlgorithmsSelector<V, E> includingAllVertices();
+
+}
